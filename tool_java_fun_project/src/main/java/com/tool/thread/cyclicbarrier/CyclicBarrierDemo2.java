e73784c4b5747c9812639cfd6fac6da2d935c4ff
zhouchenhua <noatnu@foxmail.com>
2022-12-05 11:37:28 +0800

    fix

--- tool_java_fun_project/src/main/java/com/tool/thread/cyclicbarrier/CyclicBarrierDemo2.java
+++ tool_java_fun_project/src/main/java/com/tool/thread/cyclicbarrier/CyclicBarrierDemo2.java
@@ -1,32 +1,35 @@
 package com.tool.thread.cyclicbarrier;
 
+import java.util.Arrays;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CyclicBarrierDemo2 {
+    private static ConcurrentLinkedQueue<String> linkedQueue = new ConcurrentLinkedQueue<>();
 
     public static void main(String[] args) {
         String[] names = new String[]{"小罗伯特·唐尼", "艾玛·沃特森", "艾玛·斯通", "斯嘉丽·约翰逊", "艾薇儿", "阿兰·德龙", "布雷特·道顿", "莫蕾娜·巴卡林", "费尔南达·塔瓦雷", "杰夫·高布伦", "纳帅尼尔·布佐尼"};
         AtomicInteger atomicInteger = new AtomicInteger(0);
+
         CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> {
             System.out.println("接待" + atomicInteger.incrementAndGet() + "桌");
+            System.out.println(Arrays.toString(linkedQueue.toArray()));
+            linkedQueue.clear();
         });
         for (String name : names) {
-            try {
-                //这里一定延迟一下
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            new Worker(name, cyclicBarrier).start();
-
-//            Worker worker = new Worker(name, cyclicBarrier);
-//            worker.start();
+//            try {
+//                //这里一定延迟一下
+//                Thread.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            new Worker(name, cyclicBarrier).start();
+            Worker worker = new Worker(name, cyclicBarrier);
+            worker.start();
         }
     }
-
-
 
 
     public static class Worker extends Thread {
@@ -42,9 +45,9 @@ public class CyclicBarrierDemo2 {
         @Override
         public void run() {
             try {
-                System.out.println(this.firstName + "就位");
+//                System.out.println(this.firstName + "就位");
                 this.objKey.await();
-                System.out.println();
+                linkedQueue.add(this.firstName);
                 System.out.println(this.firstName + "开始就餐");
             } catch (InterruptedException e) {
                 e.printStackTrace();
